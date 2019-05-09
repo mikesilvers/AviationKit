30d05142a75d4550903add6c7cdd99ba8c9f05fa
Mike Silvers <mikesilvers@gmail.com>
2019-05-09 11:51:09 -0400

    Commented the SkyConditions object

--- AviationKit/Classes/SkyCondition.swift
+++ AviationKit/Classes/SkyCondition.swift
@@ -7,11 +7,16 @@
 
 import Foundation
 
+/**
+ This struct contains the sky condition information.
+ */
 public struct SkyCondition : Codable {
     
+    // MARK: variables to define the sky condition
     var skyCover  : String?
     var cloudBase : Int?
     
+    // MARK: Coding Key enum
     enum CodingKeys: String, CodingKey {
         case skyCover  = "sky_cover"
         case cloudBase = "cloud_base_ft_agl"
