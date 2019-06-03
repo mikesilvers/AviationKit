7035757fda657c41e3ae029d444a7773c403d0ad
Mike Silvers <mikesilvers@gmail.com>
2019-06-03 15:33:05 -0400

    Working on the METAR reports

---
+++ AviationKit/Classes/Requests/Ext.ReportCore/Ext.Reports.Metar.swift
@@ -0,0 +1,20 @@
+//
+//  Ext.Comms.Metar.swift
+//  AviationKit
+//
+//  Created by Mike Silvers on 6/2/19.
+//
+
+import Foundation
+
+extension ReportCore {
+
+    mutating func setupMetar(_ params:MetarParams) {
+        
+        // refer to: https://www.aviationweather.gov/dataserver/example?datatype=metar
+        
+        // lets start creating our 
+        
+    }
+
+}
---
+++ AviationKit/Classes/Requests/Ext.ReportCore/Ext.Reports.Station.swift
@@ -0,0 +1,18 @@
+//
+//  Ext.Reports.Metar.swift
+//  AviationKit
+//
+//  Created by Mike Silvers on 6/2/19.
+//
+
+import Foundation
+
+extension ReportCore {
+
+    mutating func setupStation(_ params:StationInfoParams) {
+        
+        
+        
+    }
+
+}
