296cf47dcc75dc820520d3b72f4059ca952a8a03
Mike Silvers <mike@clearcodex.com>
2019-06-05 11:50:12 -0400

    Working on the tests

--- AviationKit/Classes/Networking/Reports.swift
+++ AviationKit/Classes/Networking/Reports.swift
@@ -17,27 +17,51 @@ public struct Reports {
     public init () {}
     
     //MARK: - Generic Get Functions
-    public func getReport<T>(_ params: T,_ completion: @escaping (_ result: [Any]?, Error?)->()) {
-        
+    @discardableResult
+    public func getReport<T>(_ params: T, _ testing: Bool? = false,_ completion: @escaping (_ result: [Any]?, Error?)->()) -> String? {
+
         switch params {
         case is MetarParams:
             
             var gmc = ReportCore()
                 gmc.getReport(params)
             
-            // perform the actual get request using the networking functionality
-            ProcessHTTP.shared.makeRequest(gmc,
-                                           responseType: METAR.self,
-                                           requestBody: EmptyJSON(),
-                                           xmlParser: MetarParser(), nil) { (nil, result, response, data, error) in
-                                            
-                                            if let res = result as? [METAR] {
-                                                completion(res, error)
-                                            }
+            // the testing parameter is used for testing purposes - returns the URL and does not run the process
+            if let t = testing, t == true {
+                
+                var url = gmc.baseUrl
+                if let urlParms = gmc.urlParams {
+                    url.appendQueryItems(urlParms)
+                }
+                
+                if let offsetLimit = gmc.offsetLimit {
+                    url.appendQueryItem(key: "offset", value: "\(offsetLimit.offset)")
+                    url.appendQueryItem(key: "limit", value: "\(offsetLimit.limit)")
+                }
+                
+                return url.absoluteString
+                
+            } else {
+
+                // perform the actual get request using the networking functionality
+                ProcessHTTP.shared.makeRequest(gmc,
+                                               responseType: METAR.self,
+                                               requestBody: EmptyJSON(),
+                                               xmlParser: MetarParser(), nil) { (nil, result, response, data, error) in
+                                                
+                                                if let res = result as? [METAR] {
+                                                    completion(res, error)
+                                                }
+                }
+
             }
+            
 
         default:
             completion(nil, nil)
         }
+        
+        return nil
+        
     }
 }
--- Example/Tests/METARTests.swift
+++ Example/Tests/METARTests.swift
@@ -11,14 +11,78 @@ import AviationKit
 
 class METARTests: XCTestCase {
 
-    func testExample() {
+    func testMETARRequestQueryString() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssert(true)
         
+        let reports = Reports()
+        var metarparms = MetarParams()
         
-        
-        
-    }
+        // test the default parameters for the request
+        var urlstring = reports.getReport(metarparms, true) { (result, error) in }
+        if let urlstring = urlstring,
+            let theurl = URL(string: urlstring),
+            let components = URLComponents(url: theurl, resolvingAgainstBaseURL: false),
+            let queryItems = components.queryItems {
+            
+                // break apart the query parms to make sure they are valid
+                for pc in queryItems {
+                
+                    // test the query parameters
+                    switch pc.name {
+                    
+                    // base items on all requests
+                    case "hoursBeforeNow":
+                        XCTAssertTrue(pc.value == "3")
+                    case "requesttype":
+                        XCTAssertTrue(pc.value == "retrieve")
+                    case "dataSource":
+                        XCTAssertTrue(pc.value == "metars")
+                    case "format":
+                        XCTAssertTrue(pc.value == "xml")
+                    
+                    default:
+                        XCTAssert(false, "The name \(pc.name) with value \(pc.value ?? "NO_VALUE") was not found in the test")
+                    }
+                }
+            
+        } else {
+            XCTAssert(false, "The URL query params were not parsed")
+        }
 
+        // test for
+        metarparms = MetarParams()
+        
+        urlstring = reports.getReport(metarparms, true) { (result, error) in }
+        if let urlstring = urlstring,
+            let theurl = URL(string: urlstring),
+            let components = URLComponents(url: theurl, resolvingAgainstBaseURL: false),
+            let queryItems = components.queryItems {
+            
+            // break apart the query parms to make sure they are valid
+            for pc in queryItems {
+                
+                // test the query parameters
+                switch pc.name {
+                    
+                // base items on all requests
+                case "hoursBeforeNow", "requesttype", "dataSource", "format":
+                    XCTAssert(true)
+
+                // test the remainder of the items....
+                
+                    
+                    
+                default:
+                    XCTAssert(false, "The name \(pc.name) with value \(pc.value ?? "NO_VALUE") was not found in the test")
+                }
+            }
+            
+        } else {
+            XCTAssert(false, "The URL query params were not parsed")
+        }
+
+    }
+    
 }
